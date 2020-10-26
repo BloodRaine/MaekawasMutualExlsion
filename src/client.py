b2046f107f10ac2282e958ec5f109a01cb9bdfa2
Robinson Merillat <robinson.merillat@gmail.com>
2020-10-26 12:40:45 -0600

    working multicast

--- src/client.py
+++ src/client.py
@@ -1,18 +1,33 @@
 import socket
+import struct
 
 HOST = socket.gethostname()  # The server's hostname or IP address
 PORT = 65432                 # The port used by the server
+multicast_group = ('224.3.29.71', PORT)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.settimeout(0.2)
+# s.connect(('',PORT))
+
+ttl = struct.pack('b', 1)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 message = input('-> ')
 
 while message.lower().strip() != "q":
-    
-    s.sendall(message.encode('utf-8'))
-    data = s.recv(1024).decode()
-    print('Received from server: ' + data)
-    message = input('-> ')
+    s.sendto(message.encode('utf-8'), multicast_group)
 
+    while True:
+        try:
+            data, address = s.recvfrom(1024)
+        except socket.timeout:
+            print("Timeout")
+            break
+        else:
+            print('Received from server: ' + str(data))
+
+
+        message = input('-> ')
+print("closing socket")
 s.close()
--- src/server.py
+++ src/server.py
@@ -1,17 +1,23 @@
 import socket
+import struct
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+multicast_group = '224.3.29.71'
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+server_address = ('', PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    print("Server Started")
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(server_address)
+
+group = socket.inet_aton(multicast_group)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+print("Server Started")
+
+while True:
+    print("waiting")
+    data, addr = s.recvfrom(1024)
+    print("recieved")
+    s.sendto(data, addr)
+    print("sent response")
